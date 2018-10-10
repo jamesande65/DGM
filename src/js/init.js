fa3a097a6921e77fb1fcd62d65885b77a80042ca
Dmitry <james.ande65@gmail.com>
2018-10-10 18:24:44 +0300

    icon fonts added

--- src/js/init.js
+++ src/js/init.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-
+    // var categoryBtns = $('.categories__block-item');
+    //
+    // for (var i = 0; i < categoryBtns.length; i++) {
+    //     categoryBtns[i].addEventListener('click', paintIcon);
+    // }
+    //
+    // function paintIcon (e) {
+    //     if (e.target.classList.contains('categories__block-item')) {
+    //         var value = e.target.children[0].classList.value;
+    //         value = value + "_w";
+    //         e.target.children[0].classList.value = value;
+    //     } else {
+    //         var value = e.target.parentNode.children[0].classList.value;
+    //         value = value + "_w";
+    //         e.target.parentNode.children[0].classList.value = value;
+    //     }
+    // }
 });
