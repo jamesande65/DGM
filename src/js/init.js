0e5a4c3c5455c9977bbcdc4bd1cb38fe8988708d
Dmitry <james.ande65@gmail.com>
2018-10-16 15:30:50 +0300

    last changes 16.10

--- src/js/init.js
+++ src/js/init.js
@@ -1,19 +1,50 @@
 $(document).ready(function(){
-    // var categoryBtns = $('.categories__block-item');
-    //
-    // for (var i = 0; i < categoryBtns.length; i++) {
-    //     categoryBtns[i].addEventListener('click', paintIcon);
-    // }
-    //
-    // function paintIcon (e) {
-    //     if (e.target.classList.contains('categories__block-item')) {
-    //         var value = e.target.children[0].classList.value;
-    //         value = value + "_w";
-    //         e.target.children[0].classList.value = value;
-    //     } else {
-    //         var value = e.target.parentNode.children[0].classList.value;
-    //         value = value + "_w";
-    //         e.target.parentNode.children[0].classList.value = value;
-    //     }
-    // }
+
+    $('.big-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.small-carousel',
+        arrows: false,
+        fade: true,
+        infinite: false,
+    });
+    $('.small-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        swipeToSlide: true,
+        infinite: false,
+        focusOnSelect: true,
+        asNavFor: '.big-carousel'
+    });
+
+    //handle video in carousel
+
+    var video = document.querySelectorAll(".video video");
+
+    if (video.length) {
+        handleVideo();
+    }
+
+    function handleVideo() {
+        var smallCarouselItem = $('.small-carousel .carousel-item');
+        var bigCarouselItem = $('.big-carousel .carousel-item');
+
+        for (var i = 0; i < smallCarouselItem.length; i++) {
+            if (smallCarouselItem[i].classList.contains('video')) {
+                smallCarouselItem[i].addEventListener('click', function (e) {
+                    for (var j = 0; j < bigCarouselItem.length; j++) {
+                        if (bigCarouselItem[j].children[0].nodeName === 'VIDEO') {
+                            bigCarouselItem[j].children[0].pause();
+                        }
+                    }
+                    var currentVideo = $('.big-carousel .video[data-slick-index="'+e.target.dataset.slickIndex+'"] video')[0];
+                    if (currentVideo) {
+                        currentVideo.play();
+                    }
+                });
+            }
+        }
+    }
+
 });
